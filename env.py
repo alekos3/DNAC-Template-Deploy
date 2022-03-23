63eb9b7c32d4d79bf2990ef30b90e586f47d315c
alekos3 <79263622+alekos3@users.noreply.github.com>
2022-03-23 11:08:21 -0400

    Add files via upload

---
+++ env.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+env = {
+    "base_url": "https://10.10.10.20", #dna center url IP or FQDN
+    "username": "admin", #dna center username
+    "password": "cisco123", #dna center password
+    "version": "2.2.2.8"   #dna center version
+}
