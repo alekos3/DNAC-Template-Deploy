e19856f9d3a28b67b2112431e0524bf66ef939f6
alekos3 <79263622+alekos3@users.noreply.github.com>
2022-04-15 17:31:56 -0400

    Update main.py
    Introduced uniformity between the different menus for consistency and ease of use.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-#Alexios Nersessian
+# Alexios Nersessian
 
 from dnac import *
 from env import *
+
 
 ######################################
 # SCRIPT
@@ -17,44 +18,43 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 ######################################
 
-#get Auth token and save in environment variable
+# get Auth token and save in environment variable
 env['token'] = getAuthToken(env)
-x=0
-platform_Id =''
 
 if __name__ == "__main__":
-
-    #List all platforms
     all_platforms = get_Dnac_Devices(env)
     platforms = []
-    count = 0
+    template_count = 0
+    platform_Id = ''
+    temp_select = -1
+    count_plat = 0
+
     print("List of all Platform IDs available to deploy on")
-    #print(all_platforms[0]['platformId'])
 
     for i in range(len(all_platforms)):
         if all_platforms[i]['platformId'] not in platforms:
             platforms.append(all_platforms[i]['platformId'])
-            print(platforms[i-count])
+            print(count_plat, '-', all_platforms[i]['platformId'])
+            count_plat += 1
         else:
-            count += 1
-    while True:
-        platChoice = input("Please enter the platform you wish to deploy on:")
-        if platChoice not in platforms:
-            print(f"{bcolors.FAIL}Bad choice!!!{bcolors.ENDC}\n")
             continue
-        else:
+
+    while True:
+        try:
+            platChoice = platforms[int(input("Please enter the platform you wish to deploy on:"))]
             confPlat = input(f"Confirm Platform y/n {platChoice} ")
             if confPlat == 'y':
                 platform_Id = {'platformId': platChoice}
                 break
-            elif confPlat =='n':
+            elif confPlat == 'n':
                 continue
             else:
                 print(f'{bcolors.FAIL}Bad choice!!!{bcolors.ENDC}')
-    #for x in range(len(all_platforms)):
-    #    print(all_platforms[x])
+        except:
+            print(f"{bcolors.FAIL}\nNot a valid Choice!\n {bcolors.ENDC}")
 
     print()
     print()
@@ -62,52 +62,65 @@ if __name__ == "__main__":
     # Select project
     project = get_Project_names(env)
     select_project = ''
-    row_length = 3
-    for row in [project[i:i + row_length] for i in range(0, len(project), row_length)]:
-        print(row)
-    print()
-    while select_project != 'q' and select_project not in project:
-        select_project = input('Please select a project name:')
-        if select_project not in project and select_project != 'q':
-            print("Not a valid Project name! q to quit")
-        elif select_project == 'q':
+    count_proj = 0
+    projectList = []
+
+    for proj in project:
+        print(f'{count_proj} - {proj}')
+        projectList.append(proj)
+        count_proj += 1
+
+    while True:
+        try:
+            index = int(input(f'{bcolors.OKGREEN}Please select a project 0 - {count_proj - 1}:\n{bcolors.ENDC}'))
+            select_project = projectList[index]
+            yeorne = input(f"{bcolors.WARNING}Are you sure? y/n {select_project} {bcolors.ENDC}")
             break
+        except:
+            print(f"{bcolors.FAIL}\nNot a valid Project name!\n {bcolors.ENDC}")
 
     print()
     print()
 
-    #Select Template
+    # Select Template
     if select_project != 'q':
-        template = get_Template_ID(env, select_project) #Get template ID
+        template = get_Template_ID(env, select_project)  # Get template ID
 
-        for i in range(0,len(template),2):
-            print(x, template[i], "Template ID:", template[i+1])
-            x+=1
+        for i in range(0, len(template), 2):
+            print(template_count, '-', template[i], "|  Template ID:", template[i + 1])
+            template_count += 1
 
     print()
-
 
     while select_project != 'q':
+        print()
 
-        print(f"{bcolors.OKGREEN}Please Select a template to deploy 0-{x - 1}{bcolors.ENDC}")
+        try:
+            temp_select = input(
+                f"{bcolors.OKGREEN}Please Select a template to deploy 0-{template_count - 1}{bcolors.ENDC}: ")
+            if int(temp_select) <= template_count and int(temp_select) >= 0:
+                convertSelection = int(temp_select) + int(
+                    temp_select) + 1  # Convert to proper index in list to pull correct ID
+                yeorne = input(
+                    f"Are you sure you want to deploy {bcolors.WARNING}{template[convertSelection - 1]}{bcolors.ENDC}? y or n\nq to quit")
+                if yeorne == "y":
+                    devices = get_Devices_By_Platform(env, platform_Id)
+                    group_size = 50  # group size must be below 100, 40-50 is recommended per API call.
+                    groups = [devices[x:x + group_size] for x in
+                              range(0, len(devices), group_size)]  # break down list to groups of X devices
 
-        selection = int(input())
-        convertSelection = selection + selection + 1  # Convert to proper index in list to pull correct ID
-        yeorne = input(f"Are you sure you want to deploy {bcolors.WARNING}{template[convertSelection-1]}{bcolors.ENDC}? y or n\nq to quit")
-
-
-        if yeorne == "y":
-            devices = get_Devices_By_Platform(env, platform_Id)
-            group_size = 2 #group size must be below 100, 40-50 is recommended per API call.
-            groups = [devices[x:x + group_size] for x in
-                      range(0, len(devices), group_size)]  # break down list to groups of X devices
-            for i in range(len(groups)):
-                print(groups[i])
-                deploy_Template(env, template[convertSelection], groups[i])  # Template ID is the second parameter
-            #print(template[convertSelection])
-            break
-        elif yeorne == 'q':
-            break
-        else:
+                    for i in range(len(groups)):
+                        print(groups[i])
+                        ###deploy_Template(env, template[convertSelection], groups[i])  # Template ID is the second parameter
+                    # print(template[convertSelection])
+                    break
+                elif yeorne == 'q':
+                    break
+                else:
+                    continue
+            else:
+                print(f'{bcolors.FAIL}Bad choice!!!{bcolors.ENDC}')
+                continue
+        except:
+            print(f'{bcolors.FAIL}Bad choice!!!{bcolors.ENDC}')
             continue
-
