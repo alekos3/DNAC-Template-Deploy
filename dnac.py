0b4d7bbff23f4a92f1e47e9514c044c1fe5abde6
Alexios Nersessian <79263622+alekos3@users.noreply.github.com>
2025-02-06 16:54:25 -0500

    Update dnac.py
    massive refactor

--- dnac.py
+++ dnac.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-
+__author__ = "Alexios Nersessian"
+__copyright__ = "Copyright 2025, Cisco"
+__email__ = "anersess@cisco.com"
+__version__ = "v2.0"
 import requests
-import time
-import pprint
-import urllib3
-urllib3.disable_warnings()
 
-
-def getAuthToken(env):
+def get_auth_token(env):
     """
 	Intent-based Authentication API call
 	The token obtained using this API is required to be set as value to the X-Auth-Token HTTP Header
@@ -20,49 +18,38 @@ def getAuthToken(env):
     response = requests.post(url, auth=(env['username'], env['password']), verify=False)
 
     # Validate Response
-    if 'error' in response.json():
+    if 'error' in response.text:
         print('ERROR: Failed to retrieve Access Token!')
         print('REASON: {}'.format(response.json()['error']))
 
     else:
         return response.json()['Token']  # return only the token
 
-def get_Devices_By_Platform(env, platform_Id):
-    url = '{}/dna/intent/api/v1/network-device'.format(env['base_url'])
+
+def get_device_list(env, platform_id=None):
     headers = {
         'x-auth-token': env['token'],
         'Content-Type': 'application/json',
         'Accept': 'application/json'
     }
-    devList = []
-    query_string_params = platform_Id# GET Target Devices from env file
-    response = requests.get(url, headers=headers,
-                            params=query_string_params, verify=False)
-    for item in response.json()['response']:
-        devList.append({'hostName':item['hostname'],'type': 'MANAGED_DEVICE_UUID','id':item['id']})#structure the list to the format of targetInfo
 
-    return devList
+    device_list = []
+    offset = 1
+    limit = 500  # Do NOT exceed 500 as the limit (Per CC documentation)
 
-def get_Dnac_Devices(env):
-    url = '{}/dna/intent/api/v1/network-device'.format(env['base_url'])
-    headers = {
-        'x-auth-token': env['token'],
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
-    # Make the GET Request
-    response = requests.get(url, headers=headers, verify=False)
+    while True:
+        if platform_id:
+            url = f"{env['base_url']}/dna/intent/api/v1/network-device?platformId={platform_id}&offset={offset}&limit={limit}"
+        else:
+            url = f"{env['base_url']}/dna/intent/api/v1/network-device?&offset={offset}&limit={limit}"
+        response = requests.get(url, headers=headers, verify=False)
 
-    # Validate Response
-    if 'error' in response.json():
-        print('ERROR: Failed to retrieve Network Devices!')
-        print('REASON: {}'.format(response.json()['error']))
-        return []
-    else:
-        return response.json()['response']  # return the list of dnac devices
-
-    # Need the response to check task progress
-    return response
+        if response.json().get('response'):
+            device_list.extend(response.json()['response'])
+            offset += limit
+        else:
+            break
+    return device_list
 
 
 def get_Project_names(env):
@@ -81,62 +68,41 @@ def get_Project_names(env):
     return project
 
 
-def get_Template_ID(env, project):
+def get_template_id(env, project):
     url = "{}/dna/intent/api/v1/template-programmer/template".format(env["base_url"])
-    template = []
+    templates = []
     headers = {
         "x-auth-token": env["token"],
         "Content-Type": "application/json",
     }
     response = requests.request("GET", url, headers=headers, verify=False)
 
-    #print(json.dumps(response.json(), indent=4))
     for line in response.json():
-        if project in str(line): #Get template ID from desired project
-            template.append(dict(line)['name'])
-            template.append(dict(line)['templateId'])
+        if project in str(line):  # Get template ID from desired project
+            templates.append((dict(line)['name'], dict(line)['templateId']))
 
-    return template
+    return templates
 
-############BELOW IS THE DEPLOY TEMPLATE###############################
-def deploy_Template(env, template_id, devices): #hostname, device_id,
-    url = "{}/dna/intent/api/v1/template-programmer/template/deploy".format(env["base_url"])
+
+############BELOW IS THE DEPLOY TEMPLATE###########################
+def deploy_template(env, template_id, devices):
+    url = f"{env['base_url']}/dna/intent/api/v1/template-programmer/template/deploy"
     headers = {
         "x-auth-token": env["token"],
         "Content-Type": "application/json",
     }
-    #devices = json.loads(get_Devices_By_Platform(env))
+
     payload = {
-        "targetInfo": devices, #within this variable we can pass up to 100 devices to be provisioned at once
+        "forcePushTemplate": True,
+        "targetInfo": devices,
         "templateId": template_id
     }
 
     # Make POST request
     response = requests.post(url, headers=headers, json=payload, verify=False)
 
-    # Check deployment status
-    if response.status_code == 200 or response.status_code == 202:
-        pprint.pprint(response.json())
+    return response.json().get('deploymentId')
 
-        if "not deploying" in response.json()["deploymentId"]:
-            print("Device %s not applicable for deployment of template %s. Hence, not deploying" % (
-                'hostname', template_id))
-        else:
-            print("Successfully deployed template %s to %s" % (template_id, 'hostname'))
-
-            while True:
-                if "IN_PROGRESS" == check_Deployment_Status(env,
-                                                            response.json()["deploymentId"].partition(
-                                                                "Template Deployemnt Id: ")[2]):
-                    time.sleep(2)
-                    continue
-                else:
-                    break
-
-            return response.json()
-
-    else:
-        print("Did not deploy template %s to %s" % (template_id, response.status_code))#If failed show failed devices
 
 
 def get_Task(env, task_id):
@@ -152,17 +118,88 @@ def get_Task(env, task_id):
     return response.json()  # return response with information about specific task
 
 
-# Need a way to check if deployment of template to device was successful
-def check_Deployment_Status(env, deployment_id):
-    url = "{}/dna/intent/api/v1/template-programmer/template/deploy/status/{}".format(env["base_url"], deployment_id)
+def check_deployment_status(env, deployment_id):
+    url = f"{env['base_url']}/dna/intent/api/v1/template-programmer/template/deploy/status/{deployment_id}"
     headers = {
         "x-auth-token": env["token"],
         "Content-Type": "application/json",
     }
 
-    # Make GET request
     response = requests.get(url, headers=headers, verify=False)
-    pprint.pprint(response.json())
 
-    return response.json()["devices"][0]["status"]  # return the deployment status
+    return response.json()["devices"]  # return the deployment status
 
+
+def get_device_info_by_id(env, dev_id):
+    url = f'{env["base_url"]}/dna/intent/api/v1/network-device/{dev_id}/'
+
+    headers = {
+        'x-auth-token': env['token'],
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    response = requests.request("GET", url, headers=headers, verify=False)
+
+    return response.json()
+
+
+def create_tag(env, name):
+    url = f'{env["base_url"]}/dna/intent/api/v1/tag'
+
+    headers = {
+        'x-auth-token': env['token'],
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+
+    payload = {"name": name}
+
+    response = requests.request("POST", url, headers=headers, json=payload, verify=False)
+
+    return response.status_code
+
+
+def get_tag_id(env, name):
+    url = f'{env["base_url"]}/dna/intent/api/v1/tag'
+
+    headers = {
+        'x-auth-token': env['token'],
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+
+    params = {'name': name}
+    response = requests.request("GET", url, headers=headers, params=params, verify=False)
+
+    return response.json()['response'][0]['id']
+
+
+def tag_add_member(env, tag_id, members):
+    url = f'{env["base_url"]}/dna/intent/api/v1/tag/{tag_id}/member'
+
+    headers = {
+        'x-auth-token': env['token'],
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    payload = {"networkdevice": members}
+    response = requests.request("POST", url, headers=headers, json=payload, verify=False)
+
+    return response.status_code
+
+
+def get_device_id(env, name):
+    dev_list = []
+    url = f'{env["base_url"]}/dna/intent/api/v1/device-detail'
+    headers = {
+        'x-auth-token': env['token'],
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+
+    for device in name:
+        params = {'searchBy': device[0], 'identifier': 'nwDeviceName'}
+        dev = requests.get(url, headers=headers, params=params, verify=False)
+        dev_list.append(dev.json()['response']["nwDeviceId"])
+
+    return dev_list
