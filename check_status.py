930fe6be44d6a8f3f15cc85ddd83566af02a4a1c
Alexios Nersessian <79263622+alekos3@users.noreply.github.com>
2025-02-07 10:22:47 -0500

    minor cleanup

--- check_status.py
+++ check_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 __author__ = "Alexios Nersessian"
-__copyright__ = "Copyright 2024, Cisco"
+__copyright__ = "Copyright 2025, Cisco"
 __email__ = "anersess@cisco.com"
 __version__ = "v2.0"
 
@@ -16,7 +16,7 @@ urllib3.disable_warnings()
 
 def write_results(results_csv, failed_devs, success_devs):
     try:
-        with open(results_csv, 'w', newline='') as csvfile:
+        with open(results_csv, "w", newline="") as csvfile:
             # creating a csv writer object
             csvwriter = csv.writer(csvfile)
             csvwriter.writerow(["Hostname", "Result", "Details"])
@@ -26,8 +26,8 @@ def write_results(results_csv, failed_devs, success_devs):
             csvwriter.writerows(success_devs)
 
     except FileNotFoundError:
-        os.mkdir('./results')
-        with open(results_csv, 'w', newline='') as csvfile:
+        os.mkdir("./results")
+        with open(results_csv, "w", newline="") as csvfile:
             # creating a csv writer object
             csvwriter = csv.writer(csvfile)
             csvwriter.writerow(["Hostname", "Result", "Details"])
@@ -43,13 +43,13 @@ def main():
     env["base_url"] = input("Enter Catalyst Center URL:  ").strip("/")
     env["username"] = input("Enter your username:  ")
     env["password"] = getpass.getpass()
-    env['token'] = get_auth_token(env)
+    env["token"] = get_auth_token(env)
     id_list = []
-    yeorne = ''
+    yeorne = ""
     success_devs = []
     jobs = []
     failed_devs = []
-    directory = './deployids'
+    directory = "./deployids"
     count_deploy = 0
 
     print()
@@ -57,7 +57,7 @@ def main():
     # get template to deploy from file
     for filename in os.listdir(directory):
         if filename.endswith(".csv"):
-            print(f'{count_deploy} - {filename}')
+            print(f"{count_deploy} - {filename}")
             jobs.append(filename)
             count_deploy += 1
     print()
@@ -69,13 +69,13 @@ def main():
             break
 
         except:
-            print('Not a valid choice!')
+            print("Not a valid choice!")
 
-    with open(f'{directory}/{jobs[int(select)]}', 'r', newline='') as csvfile:
-        csv_reader = csv.reader(csvfile, delimiter=',')
+    with open(f"{directory}/{jobs[int(select)]}", "r", newline="") as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=",")
 
         for row in csv_reader:
-            if row[0] != 'deploying':
+            if row[0] != "deploying":
                 id_list.append(row)
 
     for x in range(len(id_list)):
@@ -83,25 +83,26 @@ def main():
         for i in range(len(status)):
 
             while True:
-                if "IN_PROGRESS" in status[i]['status']:
+                if "IN_PROGRESS" in status[i]["status"]:
                     time.sleep(1)  # Wait 1 Sec and recheck device status
                     status = check_deployment_status(env, id_list[x][0])
                 else:
                     break
 
-            if 'SUCCESS' in status[i]['status']:
+            if "SUCCESS" in status[i]["status"]:
                 success_devs.append(
-                    [get_device_info_by_id(env, status[i]['deviceId'])['response']['hostname'], status[i]['status'],
-                     status[i]['detailedStatusMessage'].split("Message:")[-1].replace("<br></pre>", "")])
+                    [get_device_info_by_id(env, status[i]["deviceId"])["response"]["hostname"], status[i]["status"],
+                     status[i]["detailedStatusMessage"].split("Message:")[-1].replace("<br></pre>", "")])
 
             else:
                 failed_devs.append(
-                    [get_device_info_by_id(env, status[i]['deviceId'])['response']['hostname'], status[i]['status'],
-                     status[i]['detailedStatusMessage'].split("Message:")[-1].replace("<br></pre>", "")])
+                    [get_device_info_by_id(env, status[i]["deviceId"])["response"]["hostname"], status[i]["status"],
+                     status[i]["detailedStatusMessage"].split("Message:")[-1].replace("<br></pre>", "")])
 
-    results_csv = f'./results/results_{job_id}'
+    results_csv = f"./results/results_{job_id}"
 
-    write_results(results_csv, failed_devs, success_devs)
+    if failed_devs or success_devs:
+        write_results(results_csv, failed_devs, success_devs)
 
     if failed_devs:  # If there are any failed devices go ahead and ask user if they want to tag them
         while True:
@@ -114,15 +115,17 @@ def main():
                 devid = get_device_id(env, failed_devs)
                 tag_add_member(env, tagid, devid)
                 break
-            elif yeorne.lower() == 'n':
+            elif yeorne.lower() == "n":
                 break
             else:
-                print('Bad Choice!')
+                print("Invalid Choice!")
 
     print()
     print()
 
-    print(f"Deployment status has been gathered for all devices.\nCheck {results_csv} file for results.")
+    if failed_devs or success_devs:
+        print(f"Deployment status has been gathered for all devices.\nCheck {results_csv} file for results.")
+
     print(f"Total Successful devices: {len(success_devs)}")
     print(f"Total Failed devices: {len(failed_devs)}")
     print()
